2f57a5b72dcd923b169f68424542e31a3c91f3e5
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2020-11-26 15:46:43 +0700

    Update pheptinh.c

--- Bài tập 1/pheptinh.c
+++ Bài tập 1/pheptinh.c
@@ -5,12 +5,15 @@ int main(){
   int a = 3,b = 5;
   //khai bao nhieu bien
   int tong, hieu, tich;
+  //khai bao bien
   double thuong;
   tong = a + b;
   hieu = a - b;
   tich = a * b;
   thuong = (double)a / b;
   printf("a\tb\ta+b\ta-b\ta*b\ta/b\n%d\t%d\t%d\t%d\t%d\t%0.1f",a,b,tong,hieu,tich,thuong);
+  printf("\n");
+  
   
   
   
