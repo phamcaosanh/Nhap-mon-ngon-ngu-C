56c7550eff639711647019e82157aad57a6b3c68
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2020-12-03 15:20:17 +0700

    Update chieudai.c

--- Bài tập 2/chieudai.c
+++ Bài tập 2/chieudai.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void){
     int l,Met,Xentimet,Milimet;
-    printf(" Chieu dai can doi: %d", l);
+    printf(" Nhap l= ");
     scanf("%d", &l	);
  	Met=l;
     Xentimet=l*100;
