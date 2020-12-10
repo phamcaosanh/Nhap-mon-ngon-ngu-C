07f928ecd24b9c416605a7439e5ab5b9bc72e72b
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2020-12-10 16:50:54 +0700

    Update sosanh.c

--- Bài tập 3/sosanh.c
+++ Bài tập 3/sosanh.c
@@ -4,9 +4,9 @@ int main(){
 	// khai bao bien a,b
 	int a,b ;
 	printf("Nhap a =");
-	scanf("%d", & a);
+	scanf("%d", &a);
 	printf("Nhap b =");
-	scanf("%d" , & b);
+	scanf("%d" , &b);
 	// dieu kien
 	if(a > b){printf("a>b",a,b);
 	}else if(a == b) {printf("a = b",a,b);
