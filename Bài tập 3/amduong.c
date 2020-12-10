78b6be805ac0db67f663bd37361082dc266e0870
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2020-12-10 16:50:32 +0700

    Update amduong.c

--- Bài tập 3/amduong.c
+++ Bài tập 3/amduong.c
@@ -4,7 +4,7 @@ int main(){
 	// khai bao bien a
 	int a ;
 	printf("Nhap a =");
-	scanf("%d", & a);
+	scanf("%d", &a);
 	// dieu kien
 	if(a > 0){printf("%d duong",a);
 	}else if(a == 0) {printf("a = 0",a);
