f9652c9f55bfd8eb6c14ca2a3f2b7520694c8b12
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2020-12-10 15:23:41 +0700

    Create sosanh.c

---
+++ Bài tập 3/sosanh.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	// khai bao bien a,b
+	int a,b ;
+	printf("Nhap a =");
+	scanf("%d", & a);
+	printf("Nhap b =");
+	scanf("%d" , & b);
+	// dieu kien
+	if(a > b){printf("a>b",a,b);
+	}else if(a == b) {printf("a = b",a,b);
+	}else {printf("a<b",a,b);}	
+	
+  getchar();
+  return 0;
+}
