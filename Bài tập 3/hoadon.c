bf5a0358f6d1c79783c9fa1884addf6ab104c2ca
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2020-12-17 14:13:38 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -4,18 +4,17 @@ int main(){
 	// khai bao bien
 	int dongia,soluong ;
 	float thanhtien;
-	printf("Nhap soluong =");
-	scanf("%d", &dongia);
 	printf("Nhap dongia =");
+	scanf("%d", &dongia);
+	printf("Nhap soluong =");
 	scanf("%d", &soluong);
 	thanhtien=soluong*dongia;
 	// dieu kien
 	
-	if(soluong>=5 ){
-	thanhtien=(soluong*dongia)*0.75;
+	if(soluong >= 5 & dongia >= 500 ){
+	thanhtien=(soluong*dongia)*0.5;
 	printf("%0.1f",thanhtien);
-	}
-	else {printf("%d",thanhtien);}	
+	}else {printf("%d",thanhtien);}	
   getchar();
   return 0;
 }
