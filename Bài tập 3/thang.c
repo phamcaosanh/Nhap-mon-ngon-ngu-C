073fb07d66b945c77049166faa8fc8aa9dc3892a
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2020-12-17 17:03:31 +0700

    Update thang.c

--- Bài tập 3/thang.c
+++ Bài tập 3/thang.c
@@ -8,41 +8,22 @@ int main() {
 	switch(thang)
     {   
     	case 1:
-    		  printf("31 ngay");
+      case 3:
+    	case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:printf("31 ngay");
     		  break;
       case 2:
           printf("28 hoac 29 ngay");
           break;
-      case 3:
-          printf("31 ngay");
-          break;
       case 4:
-          printf("30 ngay");
-          break;
-      case 5:
-    	  printf("31 ngay");
-          break;
-      case 6:
-    	  printf("30 ngay");
-    	  break;
-      case 7:
-    	  printf("31 ngay");
-    	  break;
-      case 8:
-    	  printf("31 ngay");
-    	  break;
-      case 9:
-    	  printf("30 ngay");
-          break;
-      case 10:
-    	  printf("31 ngay");
-          break;
-      case 11:
-          printf("30 ngay");
-    	  break;
-      case 12:
-    	  printf("31 ngay");
-    	  break;
+      case 6:    		 
+    	case 9:
+    	case 11:
+    		  printf("30 ngay");
+    		  break;
       default:
           printf("Mot nam co 12 thang thoi :)) ");
     }
