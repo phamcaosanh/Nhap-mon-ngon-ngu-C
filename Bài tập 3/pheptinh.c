46352a60605670c352e7e01c7f6228ef768d8567
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2020-12-17 15:57:54 +0700

    Update pheptinh.c

--- Bài tập 3/pheptinh.c
+++ Bài tập 3/pheptinh.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 int main() {
+	// khai bao nhieu bien
 	int a = 5,b = 2;
 	char pt;
+	printf("a = %d va b = %d",a,b);
+	printf("\n");
 	printf("Vui long nhap phep tinh(+,-): ");
 	scanf("%c", &pt);
 	switch(pt)
     {
         case '+':
-            printf("5 + 2 = %d",a+b);
+            printf("a + b = %d",a+b);
             break;
-
         case '-':
-            printf("5 - 2 = %d",a-b);
+            printf("a - b = %d",a-b);
             break;
         default:
             printf("Loi! toan tu  khong dung");
