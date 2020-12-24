66916ce6720dcd7cd78474f0efd5c275f959d1d2
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2020-12-24 14:23:58 +0700

    Update motdenmuoi.c

--- Bài tập 3/motdenmuoi.c
+++ Bài tập 3/motdenmuoi.c
@@ -4,12 +4,12 @@ int main() {
   int i;
   for (i = 1; i < 11; ++i)
   {
-  	printf("%d\t", i);
+  	printf("%d ", i);
   }
     printf("\n\n");
   for (i = 1; i < 11; ++i)
   {
-  	printf("%d\t", i);
+  	printf("%d ", i);
   	printf(" 1 2 3 4 5 6 7 8 9 10\n");
   }
     return 0;
