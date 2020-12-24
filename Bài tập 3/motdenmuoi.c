6a51e72bca98563dacb613f0fcd65e912b41d74d
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2020-12-24 14:18:29 +0700

    Create motdenmuoi.c

---
+++ Bài tập 3/motdenmuoi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int i;
+  for (i = 1; i < 11; ++i)
+  {
+  	printf("%d\t", i);
+  }
+    printf("\n\n");
+  for (i = 1; i < 11; ++i)
+  {
+  	printf("%d", i);
+  	printf(" 1 2 3 4 5 6 7 8 9 10\n");
+  }
+    return 0;
+}
