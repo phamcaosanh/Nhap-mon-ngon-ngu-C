e086a738f7fd1a71cf68011395b1e9b6c044ae15
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2020-12-24 16:55:36 +0700

    Create tong.c

---
+++ Bài tập 3/tong.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+  int n,c,sum=0;
+  printf("Nhap n =");
+  scanf("%d",& n);
+  printf("n=")
+  for(c = 1; c <= n; c++){
+  sum+=c;}
+
+  switch(n)
+    {
+        case '1':
+            printf("s = %d",sum,n);
+            break;
+        case '2':
+            printf("s = 1 + %d = %d",n,sum);
+            break;
+        case '3'
+			printf("s = 1 + 2 + %d = %d",n,sum);
+			break;
+		case '5'
+			printf("s = 1 + 2 +...+%d = %d",n,sum);
+			break;	    
+        default:
+            printf("s = 1 + 2 +...+%d = %d",n,sum);
+    }
+
+  getchar();
+  return 0;
+}
