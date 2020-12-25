e2c8f254e1a9d3031833cdcb12a8152535d28890
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2020-12-25 10:20:36 +0700

    Create tamgiacso2.c

---
+++ Bài tập 3/tamgiacso2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int n,a,b;
+	printf("Nhap n = ");
+	scanf("%d",& n);
+	printf("n = %d\n",n);
+	for(a = 1; a <= n; a++ ){
+		printf("\n");
+	  for(b = 1; b <= a; b++)//vong lap ben trong
+		  printf("%d",b);
+	}
+	getchar();
+	return 0;
+}
