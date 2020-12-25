693291d30814b28bdf9673890d15d671dc351a34
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2020-12-25 09:26:57 +0700

    Update tong.c

--- Bài tập 3/tong.c
+++ Bài tập 3/tong.c
@@ -4,10 +4,10 @@ int main(){
   int n,c,sum=0;
   printf("Nhap n =");
   scanf("%d",& n);
-  printf("n=")
+  printf("n = %d\n",n);
   for(c = 1; c <= n; c++){
-  sum+=c;}
-
+  sum +=c ;
+  }
   switch(n)
     {
         case '1':
@@ -16,12 +16,12 @@ int main(){
         case '2':
             printf("s = 1 + %d = %d",n,sum);
             break;
-        case '3'
-			printf("s = 1 + 2 + %d = %d",n,sum);
-			break;
-		case '5'
-			printf("s = 1 + 2 +...+%d = %d",n,sum);
-			break;	    
+        case '3':
+	     printf("s = 1 + 2 + %d = %d",n,sum);
+	     break;
+	case '4':
+             printf("s = 1 + 2 + 3 + %d = %d",n,sum);
+	     break;	    
         default:
             printf("s = 1 + 2 +...+%d = %d",n,sum);
     }
