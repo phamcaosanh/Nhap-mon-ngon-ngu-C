58592a5528338e3489e3a6ae5ac1a2bc96d83d98
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-01-07 14:13:06 +0700

    Update review_01.c

--- Bài tập 3/review_01.c
+++ Bài tập 3/review_01.c
@@ -1,41 +1,45 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
 int main(){
   // khai bao bien
   int n;
   scanf("%d", &n);
   // Dieu kien
-  if(n == 1){
-    printf("one");
-  }
-  else if(n == 2){
-    printf("two");
-  }
-  else if(n == 3){
-    printf("three");
-  }
-  else if(n == 4){
-    printf("four");
-  }
-  else if(n == 5){
-    printf("five");
-  }
-  else if(n == 6){
-    printf("six");
-  }
-  else if(n == 7){
-    printf("seven");
-  }
-  else if(n == 8){
-    printf("eight");
-  }
-  else if(n == 9){
-    printf("nine");
-  }
-  else{
-    printf("greater than 9");
-  }
-
+  switch (n){
+        case 1:
+            printf("one");
+            break;
+        case 2:
+            printf("two");
+            break;
+        case 3:
+            printf("three");
+            break;
+        case 4:
+            printf("four");
+            break;
+        case 5:
+            printf("five");
+            break;
+        case 6:
+            printf("six");
+            break;
+        case 7:
+            printf("seven");
+            break;
+        case 8:
+            printf("eight");
+            break;
+        case 9:
+            printf("nine");
+            break;
+        default:
+            printf("Greater than 9");        
+    }
+  
   getchar();
   return 0;
 }
