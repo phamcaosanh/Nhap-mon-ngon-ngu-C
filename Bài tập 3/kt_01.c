7245cd6a785c220ecdcca7ea8762ac05f05925cc
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-01-07 16:28:34 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -2,9 +2,9 @@
 int main() {
 	printf("BANG CUU CHUONG");
 	printf("\n");
-	printf("Bang nhan 2\n2*1=2\n2*2=4\n2*3=6\n\n");
-	printf("Bang nhan 3\n3*1=3\n3*2=6\n3*3=9\n\n");
-	printf("Bang nhan 4\n4*1=4\n4*2=8\n4*3=12");
+	printf("Bang cuu chuong 2\n2*1=2\n2*2=4\n2*3=6\n\n");
+	printf("Bang cuu chuong 3\n3*1=3\n3*2=6\n3*3=9\n\n");
+	printf("Bang cuu chuong 4\n4*1=4\n4*2=8\n4*3=12");
     return 0;
 
 }
