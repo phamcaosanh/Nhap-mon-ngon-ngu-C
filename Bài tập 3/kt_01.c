0d9e374204933127b6165497bd8e32ac52800106
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-01-07 16:12:40 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 int main() {
-	//khai bao bien
-	int i,j;
-	printf("\n\t\tBANG CUU CHUONG");
-	for(i = 1;i <= 10;i++){
-		printf("\n\tBang cuu chuong %d\n",i);
-		for(j = 0;j < 10;j++){
-		printf("\n\t%d x %d = %d\n",i,j,i*j);
-		}
-	}
+	printf("BANG CUU CHUONG");
+	printf("\n");
+	printf("Bang nhan 2\n2*1=2\n2*2=4\n2*3=6\n\n");
+	printf("Bang nhan 3\n3*1=3\n3*2=6\n3*3=9\n\n");
+	printf("Bang nhan 4\n4*1=4\n4*2=8\n4*3=12");
     return 0;
 
 }
