e3f8a87c49e4f790feb2c7a8546eec7d850d45f6
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-01-08 14:26:46 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -40,3 +40,58 @@ int main(){
   getchar();
   return 0;
 }
+
+
+**Cây thông
+
+#include <stdio.h>
+
+int main(){
+  int h;
+  printf("h = %d",h);
+  scanf("%d",&h);
+  switch(h){
+  	case 1:
+  		printf("*");
+  		break;
+  	case 2:
+  		printf(" * \n");
+  		printf("***");
+  		break;
+  	case 3:
+  		printf("     * \n");
+  		printf("   * * * \n");
+  		printf(" * * * * *");
+  		break;
+  	default:
+      printf("           *  \n");
+      printf("    ***  *****  *** \n");
+      printf("      ** * * * **               \n");
+      printf("        ** * **         \n");
+      printf("       *       *      \n");
+  		printf("           *  \n");
+  		printf("         * * * \n");
+  		printf("       * * * * *\n");
+  		printf("     * * * * * * *\n");
+      printf("   * * * * * * * * *\n");
+      printf("       * * * * *\n");
+  		printf("     * * * * * * *\n");
+      printf("   * * * * * * * * *\n");
+      printf("       * * * * *\n");
+  		printf("     * * * * * * *\n");
+      printf("   * * * * * * * * *\n");
+      printf("* * * * * * * * * * * * \n");
+      printf("       ********                  \n");
+      printf("       ********                  \n");
+      printf("       ********                  \n");
+      printf("       ********                  \n");
+      printf("       ********       * * * *      \n");
+      printf("    **************   *    * *      \n");
+      printf("    **************  ******  *          \n");
+      printf("    **************  ****** *           \n");
+      printf("    **************  ******             \n");
+      printf("    **************               \n");
+    }
+  getchar();
+  return 0;
+}
