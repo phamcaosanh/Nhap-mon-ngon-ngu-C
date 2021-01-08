40675562213d45808bb76f59f2d5a49a8dad360c
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-01-08 13:33:34 +0700

    Create kt_06.c

---
+++ Bài tập 3/kt_06.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+  int h;
+  printf("h = %d",h);
+  scanf("%d",& h);
+  switch(h){
+  	case 1:
+  		printf("*");
+  		break;
+  	case 2:
+  		printf(" * \n");
+  		printf("***");
+  		break;
+  	case 3:
+  		printf("     * \n");
+  		printf("   * * * \n");
+  		printf(" * * * * *");
+  		break;
+  	default:
+  		printf("       *  \n");
+  		printf("     * * * \n");
+  		printf("   * * * * *\n");
+  		printf(" * * * * * * *");
+  }
+  getchar();
+  return 0;
+}
