f0d44151c51ff7d2a8bd2b8388c3a14cd00575e4
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-01-08 14:23:19 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -6,22 +6,36 @@ int main(){
   scanf("%d",&h);
   switch(h){
   	case 1:
-  		printf("*");
+      //h = 1
+      //hang 1,1 *,0 khoang trang
+  		printf("*");//hang1
   		break;
   	case 2:
-  		printf("  * \n");
-  		printf("* * *");
+      //h = 2
+      //hang 1,1 *,1 khoang trang
+      //hang 2,3 *,0 _____//_____
+  		printf("  * \n");//hang1
+  		printf("* * *");//hang2
   		break;
   	case 3:
-  		printf("     * \n");
-  		printf("   * * * \n");
-  		printf(" * * * * *");
+      //h=3
+      //hang 1,1 *,2 khoang trang
+      //hang 2,3 *,1 _____//_____
+      //hang 3,5 *,0 _____//_____
+  		printf("    * \n");//hang1
+  		printf("  * * * \n");//hang2
+  		printf("* * * * *");//hang3
   		break;
   	default:
-  		printf("       *  \n");
-  		printf("     * * * \n");
-  		printf("   * * * * *\n");
-  		printf(" * * * * * * *");
+      //h = 4
+      //hang 1,1 *,3 khoang trang
+      //hang 2,3 *,2 ____//______
+      //hang 3,5 *,1 ____//______
+      //hang 4,7 *,0 ____//______
+  		printf("      *  \n");//hang1
+  		printf("    * * * \n");//hang2
+  		printf("  * * * * *\n");//hang3
+  		printf("* * * * * * *");//hang4
   }
   getchar();
   return 0;
