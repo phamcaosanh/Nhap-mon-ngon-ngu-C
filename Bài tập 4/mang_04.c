0ef96b52459995d2ae295e33af541cab877aa6bf
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-01-19 16:11:39 +0700

    Create mang_04.c

---
+++ Bài tập 4/mang_04.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+  //khai bao bien
+  int n,i,Tong=0;
+  printf("Nhap n : ");
+  scanf("%d", &n);
+  //Tao mang a kich thuoc n
+  int a[n];
+  //Nhap n phan tu cua mang a tu ban phim
+  for(i = 0;i < n;i++){
+  	printf("Phan tu o mang a : ");
+    scanf("%d", &a[i]);
+  }
+  //Hien thi cac phan tu cua mang a
+  printf("Cac phan tu o mang a : ");
+  for(i = 0;i < n;i++){
+  	printf(" %d",a[i]);
+  }
+  printf("\n");
+  //Tinh tong cac phan tu trong mang a
+  for(i = 0;i < n;i++){
+  	Tong = Tong + a[i];
+  }
+  //Hien thi tong cac phan tu trong mang a
+  printf("Tong cac day so = %d",Tong);
+  return 0;
+}
