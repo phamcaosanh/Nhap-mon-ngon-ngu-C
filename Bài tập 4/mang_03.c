86920a254a4ccf5ae06f66386be10c1f0ce7b48c
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-01-19 16:03:14 +0700

    Create mang_03.c

---
+++ Bài tập 4/mang_03.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+  //khai bao bien
+  int n,i;
+  printf("Nhap n : ");
+  scanf("%d", &n);
+  //Tao mang a kich thuoc n
+  int a[n];
+  //Nhap n phan tu cua mang a tu ban phim
+  for(i = 0;i < n;i++){
+  	printf("Phan tu o mang a : ");
+    scanf("%d", &a[i]);
+  }
+  //Hien thi cac phan tu cua mang a
+  printf("Cac phan tu o mang a : ");
+  for(i = 0;i < n;i++){
+  	printf(" %d",a[i]);}
+  //Hien thi cac so chan tu mang a
+  printf("\nCac so chan tu mang a : ");
+  for(i = 0;i < n; i++){
+    if(a[i]  %2 == 0){
+    printf(" %d ",a[i]);
+  }
+  }
+  //Hien thi cac so le tu mang a
+  printf("\nCac so le tu mang a :  ");
+  for(i = 0;i < n; i++){
+    if(a[i] %2 != 0){
+    printf(" %d ",a[i]);
+  }
+  }
+  //Hien thi so 0 tu mang a
+  for(i = 0;i < n; i++){
+    if(a[i]  == 0){
+    printf("\n so %d",a[i]);  
+  }
+  }
+  return 0;
+}
