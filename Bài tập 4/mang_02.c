f23f387a3ee2623ab05149e65ea88272348b4043
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-01-19 14:33:42 +0700

    Create mang_02.c

---
+++ Bài tập 4/mang_02.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+  //khai bao bien
+  int n,i;
+  printf("Nhap n : ");
+  scanf("%d", &n);
+  //Tao mang a kich thuoc n
+  int a[n];
+  //Nhap n phan tu cua mang a tu ban phim
+  for(i = 0;i < n;i++){
+  	printf("Phan tu o mang a : ");
+    scanf("%d", &a[i]);
+  }
+  //Hien thi cac phan tu cua mang a
+  printf("Cac phan tu o mang a : ");
+  for(i = 0;i < n;i++){
+  	printf(" %d",a[i]);
+  }
+  //Hien thi cac so chan tu mang a
+  for(i = 0;i < n; i++){
+    if(a[i] %2 == 0){
+    printf("\nCac so chan tu mang a  %d",a[i]);
+  }
+  }
+  return 0;
+}
