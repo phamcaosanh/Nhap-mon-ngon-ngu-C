b280b46e7ae018835a1443e37b041c8bbd3cf788
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-01-22 08:21:45 +0700

    Update ham_01.c

--- Bài tập 5/ham_01.c
+++ Bài tập 5/ham_01.c
@@ -10,3 +10,24 @@ int main(){
 int sum(int a,int b,int c){
   return a + b + c;
 }
+
+
+Nhập 3 số nguyên bất kì từ bàn phím
+//Khối khai  báo
+#include <stdio.h>
+int sum(int a,int b,int c);//Khai báo hàm sum
+//Khối Hàm main
+int main(){
+  int x ,y,z;
+  //Nhập 3 số nguyên bất kì
+  printf("Nhap 3 so nguyen\n");
+  printf("x :");scanf("%d", &x);
+  printf("y :");scanf("%d", &y);
+  printf("z :");scanf("%d", &z);
+  //Tổng 3 số nguyên
+  printf("x + y + z = %d",sum(x,y,z));
+  return 0;
+}
+int sum(int a,int b,int c){
+  return a + b + c;
+}
