821d9b92c8fe7ff8139b269b3bd37711c0545bce
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-01-23 10:06:48 +0700

    Update TH_03.c

--- Bài tập 5/TH_03.c
+++ Bài tập 5/TH_03.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-
-
+//Khoi khai bao
+float a,b,c;
+int So_lon_nhat();
+//Khoi ham main
 int main() {
-//khai bao bien
-int a,b,c;
-//Nhap 3 so a,b,c bat ki tu ban phim
 printf("Nhap 3 so bat ki tu ban phim : \n");
-printf("a : ");scanf("%d", &a);
-printf("b : ");scanf("%d", &b);
-printf("C : ");scanf("%d", &c);
-//dieu kien va xuat ra man hinh so lon nhat
-if(a<b && b<c){printf("So lon nhat %d",c);}
-if(a<c && c<b){printf("So lon nhat %d",b);}
-if(b<c && c<a){printf("So lon nhat %d",a);}
+printf("a : ");scanf("%f", &a);//Nhap a tu ban phim
+printf("b : ");scanf("%f", &b);//Nhap b tu ban phim
+printf("C : ");scanf("%f", &c);//Nhap c tu ban phim
+So_lon_nhat();
   return 0;
+}
+//Khoi dinh nghia ham
+int So_lon_nhat(){
+if(a<c && b<c){printf("%.1f",c);}
+if(a<b && c<b){printf("%.1f",b);}
+if(b<c && c<a){printf("%.1f",a);}
+return 0;
 }
