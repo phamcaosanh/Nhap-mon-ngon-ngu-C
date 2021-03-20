3d4072ca804462b2d717df27eca39433445912ef
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-03-20 14:07:01 +0700

    Create Bai_1.c

---
+++ Bài tập 5/Bai_1.c
@@ -0,0 +1,17 @@
+//khoi khai bao
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  //Khai bao bien
+  int n,S = 0;
+  //Nhap so nguyen duong n bat ky
+  printf("Nhap so nguyen duong n bat ky tu ban phim  n = ");
+  scanf("%d", &n);
+  for(int i = 1; i <= n;i++){
+    S+=i*i;
+  }
+  //Tinh tong
+  printf("S = 1^2 + 2^2+...+%d^2 = %d", n, S);
+    return 0;
+}
